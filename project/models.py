1ae32ae038f1d85b1fdfca6789bce24ab997eb58
JohnnyZhang19 <zonglin.zhang@ucalgary.ca>
2022-03-29 13:02:19 -0600

    edit

--- project/models.py
+++ project/models.py
@@ -24,7 +24,7 @@ class Category(models.Model):
     #category name
     name = models.CharField(max_length=20, verbose_name="Category name")
     #category describe
-    describe = models.CharField(max_length=100, verbose_name="descitbe", null=True)
+    describe = models.CharField(max_length=100, verbose_name="describe", null=True)
 
     def __str__(self):
         return self.name
